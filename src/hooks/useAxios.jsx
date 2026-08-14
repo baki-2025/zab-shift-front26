da77defe34a1241ecb1e46160697a6ff9936d61f
Abdullah Baki <abz.147570@gmail.com>
2026-08-14 23:28:51 +0600

    update

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 const axiosInstance = axios.create({
-    baseURL: 'https://zap-shift-server-phi.vercel.app'
+    baseURL: 'http://localhost:3000'
      //baseURL: 'https://zap-shift-server-phi.vercel.app'
 })
 
